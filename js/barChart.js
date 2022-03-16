bfd2bd8d703d8075517379b46671dfa16014b623
Samuel Scherer <swiimii962@gmail.com>
2022-03-16 14:35:56 -0400

    Progress on bar charts

---
+++ js/barChart.js
@@ -0,0 +1,114 @@
+// ES6 Class
+class BarChart {
+
+  constructor(_config, _data, _data_selection, max_bars=12, _legend) {
+    this.config = {
+      parentElement: _config.parentElement,
+      containerWidth: _config.containerWidth || 500,
+      containerHeight: _config.containerHeight || 300,
+      margin: { top: 10, bottom: 100, right: 50, left: 50 }
+    }
+
+    this.data = _data;
+    this.data_selection = _data_selection;
+    this.legend = _legend;
+
+    // Call a class function
+    this.initVis();
+  }
+    initVis() {
+      let vis = this;
+
+      vis.data_map = d3.group(vis.data, d => d[vis.data_selection]);
+
+      vis.data_selections = new Map();
+      
+      (Array.from(vis.data_map.keys()).sort(d3.ascending)).forEach((key) => {
+        vis.data_selections.set(key, vis.data_map.get(key).length);
+      });
+      
+      vis.colors = ["#8dd3c7","#bebada","#fb8072","#80b1d3","#fdb462","#b3de69","#fccde5","#d9d9d9","#bc80bd","#ccebc5","#ffed6f"];
+  
+      vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
+      vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
+  
+      vis.xScale = d3.scaleBand()
+          .range([0, vis.width])
+          .paddingInner(0.2)
+          .paddingOuter(0.2);
+      
+      vis.barColor = d3.scaleOrdinal()
+          .domain(vis.data_selections.keys())
+          .range(vis.colors)
+  
+      vis.yScale = d3.scaleLinear()
+          .range([vis.height, 0]);
+      
+      // Initialize axes
+      vis.xAxis = d3.axisBottom(vis.xScale);
+      vis.yAxis = d3.axisLeft(vis.yScale).ticks(6);
+  
+      // Define size of SVG drawing area
+      vis.svg = d3.select(document.getElementById(vis.config.parentElement))
+          .attr('width', vis.config.containerWidth)
+          .attr('height', vis.config.containerHeight);
+  
+      // Append group element that will contain our actual chart
+      vis.chart = vis.svg.append('g')
+          .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
+  
+      // Append empty x-axis group and move it to the bottom of the chart
+      vis.xAxisG = vis.chart.append('g')
+          .attr('class', 'axis x-axis')
+          .attr('transform', `translate(0,${vis.height})`);
+      
+      // Append y-axis group
+      vis.yAxisG = vis.chart.append('g')
+          .attr('class', 'axis y-axis');
+      
+      vis.updateVis();
+    }
+  
+    /**
+     * Prepare the data and scales before we render it.
+     */
+    updateVis() {
+      let vis = this;
+  
+      vis.xScale.domain(vis.data_selections.keys());
+      vis.yScale.domain([0, 100]);
+  
+      vis.renderVis();
+    }
+  
+    /**
+     * This function contains the D3 code for binding data to visual elements
+     * Important: the chart is not interactive yet and renderVis() is intended
+     * to be called only once; otherwise new paths would be added on top
+     */
+    renderVis() {
+      let vis = this;
+      
+      console.log(vis.data_selections.keys());
+      vis.chart.selectAll('category').data(vis.data_selections.keys()).enter()
+        .append('rect')
+          .attr('x', d => vis.xScale(d))
+          .attr('y', d => vis.yScale(vis.data_selections.get(d).length))
+          .attr('height', d => vis.height - vis.yScale(vis.data_selections.get(d).length))
+          .attr('width', vis.xScale.bandwidth())
+          .attr('fill', d => vis.barColor(d));
+  
+      // Update the axes
+      vis.xAxisG.call(vis.xAxis)
+        .selectAll('text')
+          .style('text-anchor','end')
+          .attr("dx", "-.8em")
+          .attr("dy", ".15em")
+          .attr("transform", "rotate(-15)");;
+      vis.yAxisG.call(vis.yAxis);
+    }
+  
+  
+  
+    
+  }
--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ d3.csv('data/occurrences.csv')
 	  d.latitude = +d.decimalLatitude; //make sure these are not strings
 	  d.longitude = +d.decimalLongitude; //make sure these are not strings
 	  d.year = d.year != '' ? +d.year : null;
+    d.month = d.month != '' ? +d.month : null;
 	  d.startDayOfYear = d.startDayOfYear != '' ? +d.startDayOfYear : null;
 	  
 	  
@@ -16,6 +17,18 @@ d3.csv('data/occurrences.csv')
 
     // Initialize chart and then show it
     leafletMap = new LeafletMap({ parentElement: '#my-map'}, data);
+
+    const myBar1 = new BarChart({
+      parentElement: 'bar1',
+    }, data, "month", 12, 'legend3');
+  
+    const myBar2 = new BarChart({
+      parentElement: 'bar2',
+    }, data, "class", 12, 'legend4');
+
+    const myBar3 = new BarChart({
+      parentElement: 'bar3',
+    }, data, "recordedBy", 12, 'legend5');
 
 
   })
