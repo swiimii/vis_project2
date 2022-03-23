d81f9c21947a6ee9331e713a47cfb4149d054e60
Jenn <97474493+jenngutman4@users.noreply.github.com>
2022-03-23 16:00:02 -0400

    Update year ticks

--- js/timeline.js
+++ js/timeline.js
@@ -16,34 +16,34 @@ class Timeline {
   }
 
   initVis() {
-  	let vis = this;
-  	vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
-  	vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
+   let vis = this;
+   vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
+   vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
 
-  	vis.svg = d3.select(vis.config.parentElement)
-  		.attr('width', vis.config.containerWidth)
-  		.attr('height', vis.config.containerHeight);
+   vis.svg = d3.select(vis.config.parentElement)
+  	.attr('width', vis.config.containerWidth)
+  	.attr('height', vis.config.containerHeight);
 
-  	vis.chart = vis.svg.append('g')
-  		.attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
+   vis.chart = vis.svg.append('g')
+  	.attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
 
-  	vis.xScale = d3.scaleBand()
-  		.range([0, vis.width])
-  		.padding(0.1);
+   vis.xScale = d3.scaleBand()
+  	.range([0, vis.width])
+  	.padding(0.1);
 
-  	vis.xAxisG = d3.axisBottom(vis.xScale).tickFormat(d3.format("d"));
-  	vis.chart.append('g')
-  		.attr("transform", `translate(0, ${vis.height})`)
-  		.attr("class", "xAxis")
+   vis.xAxisG = d3.axisBottom(vis.xScale).tickFormat(d3.format("d"));
+   vis.chart.append('g')
+  	.attr("transform", `translate(0, ${vis.height})`)
+  	.attr("class", "xAxis")
 
 
-   	vis.yScale = d3.scaleLinear()
-   		.range([vis.height,0]).nice()
+    vis.yScale = d3.scaleLinear()
+   	.range([vis.height,0])
    		//.domain([0, ]);
 
-   	vis.yAxisG = d3.axisLeft(vis.yScale);
-   	vis.chart.append('g')
-   		.attr("class", "yAxis");
+    vis.yAxisG = d3.axisLeft(vis.yScale);
+    vis.chart.append('g')
+   	.attr("class", "yAxis");
 
     vis.brush = d3.brushX()
     	.extent([[0, 0],[vis.width, vis.height]])
@@ -77,75 +77,67 @@ class Timeline {
       .attr("font-weight", "bold")
       .attr("text-anchor", "middle")
       .style("font-size", "18px")
-      .text("Number of Samples Collected Each Year")
-    
+      .text("Number of Samples Collected Each Year");
    	
-   	vis.updateVis();
+   vis.updateVis();
   }
 
   updateVis() {
-  	let vis = this;
+    let vis = this;
 
-  	vis.xScale.domain(vis.data.map(d=>d.year));
-  	vis.chart.selectAll(".xAxis")
-  		.call(vis.xAxisG)
-  		.selectAll("text")
-  			.attr("transform", "translate(-10,0)rotate(-60)")
-    		.style("text-anchor", "end");
+    vis.xScale.domain(vis.data.map(d=>d.year));
+    vis.chart.selectAll(".xAxis").call(vis.xAxisG);
 
     console.log(d3.max(vis.data.map(o => o.count)));
 
     vis.yScale.domain([0, d3.max(vis.data.map(o => o.count))]);
-    vis.chart.selectAll(".yAxis")
-  		.call(vis.yAxisG);
+    vis.chart.selectAll(".yAxis").call(vis.yAxisG);
 
-  	vis.rects = vis.chart.selectAll(".bar")
-  		.data(vis.data)
-  		.join("rect")
-  			.attr("class", "bar")
-  			.attr("x", d=>vis.xScale(d.year))
-  			.attr("y", d=>vis.yScale(d.count))
-  			.attr("width", vis.xScale.bandwidth())
-  			.attr("height", d=> vis.height - vis.yScale(d.count))
-  			.attr("fill", "steelblue")
-  		.on('mouseover', (event,d) => {
-        console.log(d);
-  			d3.select('#tooltip')
-            .style('display', 'block')
-            .style('left', (event.pageX + 10) + 'px')   
-            .style('top', (event.pageY + 10) + 'px')
-            .html(`<div class="tooltip"><strong>${d.year}</strong> - ${d.count} samples</div>`);
+    vis.rects = vis.chart.selectAll(".bar")
+  	.data(vis.data)
+  	.join("rect")
+  		.attr("class", "bar")
+  		.attr("x", d=>vis.xScale(d.year))
+  		.attr("y", d=>vis.yScale(d.count))
+  		.attr("width", vis.xScale.bandwidth())
+  		.attr("height", d=> vis.height - vis.yScale(d.count))
+  		.attr("fill", "steelblue")
+  	.on('mouseover', (event,d) => {
+        	console.log(d);
+  		d3.select('#tooltip')
+            		.style('opacity', 1)
+			.style('display', 'block')
+            		.style('left', (event.pageX + 10) + 'px')   
+            		.style('top', (event.pageY + 10) + 'px')
+            	.html(`<div class="tooltip"><strong>${d.year}</strong> - ${d.count} samples</div>`);
         })
         .on('mouseleave', () => {
-          d3.select('#tooltip').style('display', 'none');
+          d3.select('#tooltip').style('opacity', 0);
         });	
 
 
   }
 
   brushed(selection) { 
-  	let vis = this;
+    let vis = this;
 
-  	console.log(selection);
+    console.log(selection);
 
     let selectedDomain = [vis.xScale.domain()[Math.trunc(selection[0]/vis.xScale.step())], vis.xScale.domain()[Math.trunc(selection[1]/vis.xScale.step())]];
     vis.brushedData = vis.data.slice(selectedDomain[0] - 1859, selectedDomain[1] - 1859+1);
 
     vis.xScale.domain(vis.brushedData.map(d=>d.year));
+    if((selectedDomain[1] - selectedDomain[0]) > 50){
+      vis.xAxisG.tickValues(vis.xScale.domain().filter(function(d,i){ return !((d)%5)}));
+    }
+    else {
+      vis.xAxisG.tickValues(vis.xScale.domain().filter(function(d,i){ return !((d)%1)}));
+    } 
     
-    vis.chart.selectAll(".xAxis")
-      .transition()
-      .duration(1000)
-      .call(vis.xAxisG)
-      .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-60)")
-        .style("text-anchor", "end");
+    vis.chart.selectAll(".xAxis").transition().duration(1000).call(vis.xAxisG);
 
     vis.yScale.domain([0, d3.max(vis.brushedData.map(o => o.count))]);
-    vis.chart.selectAll(".yAxis")
-      .transition()
-      .duration(1000)
-      .call(vis.yAxisG);
+    vis.chart.selectAll(".yAxis").transition().duration(1000).call(vis.yAxisG);
 
     vis.rects
       .data(vis.brushedData)
