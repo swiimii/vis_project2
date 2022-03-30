7c464427a00774861f37893d1ef1636fdf0aa80e
Jenn <97474493+jenngutman4@users.noreply.github.com>
2022-03-30 14:55:10 -0400

    Add files via upload

--- js/barChart.js
+++ js/barChart.js
@@ -12,10 +12,18 @@ class BarChart {
     }
 
     this.data = _data;
+    this.filtered_data = this.data;
     this.data_selection = _data_selection;
     this.legend = _legend;
     this.sortByValue = _sortByValue;
     this.maxBars = _maxBars;
+
+    if (typeof(BarChart.DisplayedCharts) != 'object') {
+      BarChart.DisplayedCharts = Array(this);
+    }
+    else {
+      BarChart.DisplayedCharts = BarChart.DisplayedCharts.concat(this);
+    }
 
     // Call a class function
     this.initVis();
@@ -23,34 +31,6 @@ class BarChart {
   
   initVis() {
     let vis = this;
-
-    vis.data_map = d3.group(vis.data, d => d[vis.data_selection]);
-
-    vis.data_selections = new Map();
-    
-    // If a number set to a month name
-    (Array.from(vis.data_map.keys()).sort(d3.ascending)).forEach((key) => {
-      if (key != null) {
-        let visualKey = key;
-        if (`${key}` in Array.from(months.keys())) {
-          visualKey = months.get(`${key}`);
-        }
-        vis.data_selections.set(visualKey, vis.data_map.get(key).length);
-      }
-    });
-
-    if (vis.sortByValue) {
-      vis.data_selections = new Map([...vis.data_selections].sort((a,b) => b[1] - a[1]));
-    }
-
-    if (vis.maxBars && vis.maxBars > 0) {
-      const list = [...vis.data_selections.entries()];
-      const firstHalf = list.slice(0,vis.maxBars);
-      const secondHalf = list.slice(-vis.maxBars);
-      const otherTotal = d3.sum(secondHalf, d => d[1]);
-      vis.data_selections = new Map(firstHalf);
-      vis.data_selections.set("Other", otherTotal);
-    }
     
     vis.colors = ["#8dd3c7","#bebada","#fb8072","#80b1d3","#fdb462","#b3de69","#fccde5","#d9d9d9","#bc80bd","#ccebc5","#ffed6f"];
 
@@ -61,14 +41,10 @@ class BarChart {
         .range([0, vis.width])
         .paddingInner(0.2)
         .paddingOuter(0.2);
-    
-    vis.barColor = d3.scaleOrdinal()
-        .domain(vis.data_selections.keys())
-        .range(vis.colors)
 
     vis.yScale = d3.scaleLinear()
         .range([vis.height, 0]);
-    
+
     // Initialize axes
     vis.xAxis = d3.axisBottom(vis.xScale);
     vis.yAxis = d3.axisLeft(vis.yScale).ticks(6);
@@ -86,51 +62,10 @@ class BarChart {
     vis.xAxisG = vis.chart.append('g')
         .attr('class', 'axis x-axis')
         .attr('transform', `translate(0,${vis.height})`);
-    
+
     // Append y-axis group
     vis.yAxisG = vis.chart.append('g')
         .attr('class', 'axis y-axis');
-    
-    vis.updateVis();
-  }
-
-  /**
-   * Prepare the data and scales before we render it.
-   */
-  updateVis() {
-    let vis = this;
-
-    vis.xScale.domain(vis.data_selections.keys());
-    vis.yScale.domain([0, d3.max(vis.data_selections.values())]).nice();
-
-    vis.renderVis();
-  }
-
-  /**
-   * This function contains the D3 code for binding data to visual elements
-   * Important: the chart is not interactive yet and renderVis() is intended
-   * to be called only once; otherwise new paths would be added on top
-   */
-  renderVis() {
-    let vis = this;
-    
-    console.log(vis.data_selections.keys());
-    vis.chart.selectAll('category').data(vis.data_selections.keys()).enter()
-      .append('rect')
-        .attr('x', d => vis.xScale(d))
-        .attr('y', d => vis.yScale(vis.data_selections.get(d)))
-        .attr('height', d => vis.height - vis.yScale(vis.data_selections.get(d)))
-        .attr('width', vis.xScale.bandwidth())
-        .attr('fill', d => vis.barColor(d));
-
-    // Update the axes
-    vis.xAxisG.call(vis.xAxis)
-      .selectAll('text')
-        .style('text-anchor','end')
-        .attr("dx", "-.8em")
-        .attr("dy", ".15em")
-        .attr("transform", "rotate(-40)");;
-    vis.yAxisG.call(vis.yAxis);
 
     // Y Axis label
     let font_size = 12;
@@ -153,9 +88,94 @@ class BarChart {
       // These can be replaced by style if necessary
       .attr('font-family', 'sans-serif')
       .attr('font-size', font_size)
-      
-  }
     
+    vis.updateVis();
+  }
+
+  /**
+   * Prepare the data and scales before we render it.
+   */
+  updateVis(filteredData = null) {
+    let vis = this;
+    
+    if (filteredData) {
+      vis.filtered_data = filteredData;
+    } else {
+      vis.filtered_data = vis.data;
+    }
+
+    vis.xAxis = d3.axisBottom(vis.xScale);
+    vis.yAxis = d3.axisLeft(vis.yScale).ticks(6);
+
+    vis.data_map = d3.group(vis.filtered_data, d => d[vis.data_selection]);
+
+    vis.data_selections = new Map();
+    
+    // If a number set to a month name
+    (Array.from(vis.data_map.keys()).sort(d3.ascending)).forEach((key) => {
+      if (key != null) {
+        let visualKey = key;
+        if (`${key}` in Array.from(months.keys())) {
+          visualKey = months.get(`${key}`);
+        }
+        vis.data_selections.set(visualKey, vis.data_map.get(key).length);
+      }
+    });
+
+    if (vis.sortByValue) {
+      vis.data_selections = new Map([...vis.data_selections].sort((a,b) => b[1] - a[1]));
+    }
+
+    if (vis.maxBars && vis.maxBars > 0 && vis.data_selections.size > vis.maxBars) {
+      const list = [...vis.data_selections.entries()];
+      const firstHalf = list.slice(0,vis.maxBars);
+      const secondHalf = list.slice(-vis.maxBars);
+      const otherTotal = d3.sum(secondHalf, d => d[1]);
+      vis.data_selections = new Map(firstHalf);
+      vis.data_selections.set("Other", otherTotal);
+    }
+
+    vis.xScale.domain(vis.data_selections.keys());
+    vis.yScale.domain([0, d3.max(vis.data_selections.values())]).nice();
+    
+    vis.barColor = d3.scaleOrdinal()
+      .domain(vis.data_selections.keys())
+      .range(vis.colors)
+
+    vis.renderVis();
+  }
+
+  renderVis() {
+    let vis = this;
+
+    // vis.chart.selectAll('category').data(vis.data_selections.keys()).exit().remove();
+
+    vis.chart.selectAll('rect').remove();
+
+    vis.chart.selectAll('category').data(vis.data_selections.keys())
+      .join('rect')
+        .attr('x', d => vis.xScale(d))
+        .attr('y', d => vis.yScale(vis.data_selections.get(d)))
+        .attr('height', d => vis.height - vis.yScale(vis.data_selections.get(d)))
+        .attr('width', vis.xScale.bandwidth())
+        .attr('fill', d => vis.barColor(d));
+
+    // Update the axes
+    vis.xAxisG.call(vis.xAxis)
+      .selectAll('text')
+        .style('text-anchor','end')
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(-40)");;
+    vis.yAxisG.call(vis.yAxis);
+
+  }
+}
+
+function UpdateBarCharts(filteredData) {
+  BarChart.DisplayedCharts.forEach(chart => {
+    chart.updateVis(filteredData);
+  });
 }
 
 const months = new Map(Object.entries({
