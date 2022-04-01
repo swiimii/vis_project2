99f60754c8369b063d02e214595d7c6b2bfb918b
russej9 <36237942+russej9@users.noreply.github.com>
2022-04-01 14:56:26 -0400

    Add files via upload

--- js/leafletMap.js
+++ js/leafletMap.js
@@ -243,28 +243,88 @@ class LeafletMap {
     // }
    
    //redraw based on new zoom- need to recalculate on-screen position
-	vis.Dots
-		.data(vis.data)
-		.join('circle')
-		  .attr("cx", d => vis.theMap.latLngToLayerPoint([d.latitude,d.longitude]).x)
-		  .attr("cy", d => vis.theMap.latLngToLayerPoint([d.latitude,d.longitude]).y)
-		  .attr("r", vis.radiusSize)
-		  .attr("fill", function(d){
-				switch(vis.colorType)
-				{
-					case 'year':
-						return vis.colorScaleYear(d.year);
-						break;
-					case 'SD':
-						return vis.colorScaleStartDay(d.startDayOfYear);
-						break;
-					case 'class':
-						return vis.colorScaleClass(d.class);
-						break;
-					default:
-						console.log('the fuck you doing');
-				}
-				});
+	vis.Dots = vis.svg.selectAll('circle')
+                    .data(vis.data) 
+                    .join('circle')
+                        .attr("fill", function(d){
+							switch(vis.colorType)
+							{
+								case 'year':
+									return vis.colorScaleYear(d.year);
+									break;
+								case 'SD':
+									return vis.colorScaleStartDay(d.startDayOfYear);
+									break;
+								case 'class':
+									return vis.colorScaleClass(d.class);
+									break;
+								default:
+									console.log('the fuck you doing');
+							}
+							})
+                        .attr("stroke", "black")
+                        //Leaflet has to take control of projecting points. Here we are feeding the latitude and longitude coordinates to
+                        //leaflet so that it can project them on the coordinates of the view. Notice, we have to reverse lat and lon.
+                        //Finally, the returned conversion produces an x and y point. We have to select the the desired one using .x or .y
+                        .attr("cx", d => vis.theMap.latLngToLayerPoint([d.latitude,d.longitude]).x)
+                        .attr("cy", d => vis.theMap.latLngToLayerPoint([d.latitude,d.longitude]).y) 
+                        .attr("r", 3)
+                        .on('mouseover', function(event,d) { //function to add mouseover event
+                            d3.select(this).transition() //D3 selects the object we have moused over in order to perform operations on it
+                              .duration('150') //how long we are transitioning between the two states (works like keyframes)
+                              .attr("fill", "red") //change the fill
+                              .attr('r', 4); //change radius
+
+                            //create a tool tip
+                            d3.select('#tooltip')
+                                .style('display', 'block')
+                                .style('z-index', 1000000)
+                                  // Format number with million and thousand separator
+                                .html(`<div class="tooltip-label"><l> Collector: ${d.recordedBy}</l><br>
+										<l>Collection Date ${d.eventDate}</l><br>
+										<l>Classification: ${d.higherClassification}</l><br>
+										<l>Habitat: ${d.habitat}</l><br>
+										<l>Substrate: ${d.substrate}</l></div>`);
+
+                          })
+                        .on('mousemove', (event) => {
+                            //position the tooltip
+                            d3.select('#tooltip')
+                             .style('left', (event.pageX + 10) + 'px')   
+                              .style('top', (event.pageY + 10) + 'px');
+                         })              
+                        .on('mouseleave', function() { //function to add mouseover event
+                            d3.select(this).transition() //D3 selects the object we have moused over in order to perform operations on it
+                              .duration('150') //how long we are transitioning between the two states (works like keyframes)
+                              .attr("fill", function(d){
+									switch(vis.colorType)
+									{
+										case 'year':
+											return vis.colorScaleYear(d.year);
+											break;
+										case 'SD':
+											return vis.colorScaleStartDay(d.startDayOfYear);
+											break;
+										case 'class':
+											return vis.colorScaleClass(d.class);
+											break;
+										default:
+											console.log('the fuck you doing');
+									}
+									})
+                              .attr('r', 3) //change radius
+
+                            d3.select('#tooltip').style('display', 'none');//turn off the tooltip
+
+                          })
+                        .on('click', (event, d) => { 
+                        		window.open(d.references);
+                        //experimental feature I was trying- click on point and then fly to it
+                           // vis.newZoom = vis.theMap.getZoom()+2;
+                           // if( vis.newZoom > 18)
+                           //  vis.newZoom = 18; 
+                           // vis.theMap.flyTo([d.latitude, d.longitude], vis.newZoom);
+                          });
 			
 
 	}
